a557ef6184996a5b5c0542372175e3bcef04ecb3
Emil Fridriksson <emil@hlekkir.com>
2016-04-16 14:20:35 +0000

    WIP

--- src/boot.s
+++ src/boot.s
@@ -8,6 +8,22 @@
 [GLOBAL start]
 start:
     cli                         ; Disable interrupts.
+    xor eax, eax
+ 
+    ; Set up segment registers.
+    mov ss, eax
+    ; Set up stack so that it starts below start.
+    mov esp, start
+ 
+    mov ds, eax
+    mov es, eax
+    mov fs, eax
+    mov gs, eax
+    cld
+ 
+    call CheckCPU                     ; Check whether we support Long Mode or not.
+    jc .NoLongMode
+
 [BITS 64]
     call __main
     jmp $                       ; Enter an infinite loop, to stop the processor
